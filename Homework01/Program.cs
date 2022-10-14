14bd66b3ad50ec2fd51390b99996154c3e8f53eb
Vovqa <vovan102vb@gmail.com>
2022-10-14 17:29:23 +0300

    Added task #4

--- Homework01/Program.cs
+++ Homework01/Program.cs
@@ -2,7 +2,7 @@
 Задача 2.
 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 */
-
+/*
 Console.Write("Enter the first number: ");
 int number1 = Convert.ToInt32( Console.ReadLine() );
 Console.Write("Enter the second number: ");
@@ -14,7 +14,25 @@ else if( number1 < number2 )
     Console.Write($"The number {number2} is greater, the number {number1} is less.");
 else
     Console.Write("Numbers " + number1 + " and " + number2 + " are equal.");
+*/
 
+//------------------------------------------------------------------------
+/*
+Задача 4.
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+*/
 
+Console.Write("Enter the first number: ");
+int number1 = Convert.ToInt32( Console.ReadLine() );
+Console.Write("Enter the second number: ");
+int number2 = Convert.ToInt32( Console.ReadLine() );
+Console.Write("Enter the third number: ");
+int number3 = Convert.ToInt32( Console.ReadLine() );
+
+int max = number1;
+if( number2 > max  ) max = number2;
+if( number3 > max  ) max = number3;
+
+Console.Write($"The number {max} is the maximum.");
 
 
