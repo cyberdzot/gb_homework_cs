ab10caf87a87e74b285f3a921165abb9b8ebf24c
Vovqa <vovan102vb@gmail.com>
2022-10-14 17:38:42 +0300

    Added task #6

--- Homework01/Program.cs
+++ Homework01/Program.cs
@@ -21,7 +21,7 @@ else
 Задача 4.
 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 */
-
+/*
 Console.Write("Enter the first number: ");
 int number1 = Convert.ToInt32( Console.ReadLine() );
 Console.Write("Enter the second number: ");
@@ -34,5 +34,21 @@ if( number2 > max  ) max = number2;
 if( number3 > max  ) max = number3;
 
 Console.Write($"The number {max} is the maximum.");
+*/
+
+//------------------------------------------------------------------------
+/*
+Задача 6.
+Напишите программу, которая на вход принимает число и выдаёт,
+является ли число чётным (делится ли оно на два без остатка).
+*/
+
+Console.Write("Enter the number: ");
+int number = Convert.ToInt32( Console.ReadLine() );
+
+if( number % 2 == 0 )
+    Console.Write($"The number {number} is even.");
+else
+    Console.Write($"The number {number} is odd.");
 
 
