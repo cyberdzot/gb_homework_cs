1a879c7a045c0360aaa70f7c5c8f5793e4890bd9
Vovqa <vovan102vb@gmail.com>
2022-10-17 22:19:16 +0300

    Added task #10 and changes in file .gitignore

---
+++ Homework02/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 10.
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+*/
+int GetSecondDigit( int arg )
+{
+    return (arg / 10 % 10);
+}
+
+Console.Write("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32( Console.ReadLine() );
+
+Console.Write($"Вторая цифра: {GetSecondDigit( threeDigitNumber )}.");
+
+//------------------------------------------------------------------------
