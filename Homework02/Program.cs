d894157ad87c97f1d4b74d0b8d13898c9d1c1048
Vovqa <vovan102vb@gmail.com>
2022-10-17 23:53:23 +0300

    Added task #13

--- Homework02/Program.cs
+++ Homework02/Program.cs
@@ -2,14 +2,59 @@
 Задача 10.
 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 */
-int GetSecondDigit( int arg )
+/*
+int GetSecondDigit(int arg)
 {
     return (arg / 10 % 10);
 }
 
 Console.Write("Введите трёхзначное число: ");
-int threeDigitNumber = Convert.ToInt32( Console.ReadLine() );
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"Вторая цифра: {GetSecondDigit( threeDigitNumber )}.");
+Console.Write($"Вторая цифра: {GetSecondDigit(threeDigitNumber)}.");
+*/
+//------------------------------------------------------------------------
+/*
+Задача 13.
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+*/
+
+int GetCountDigits(int number)
+{
+    int i = 0;
+    int tempNum = number;
+    while (tempNum != 0)
+    {
+        tempNum = tempNum / 10;
+        i++;
+    }
+    return i;
+}
+
+int GetDigitSetPos(int pos, int number)
+{
+    int factor = 1; // множитель, если число будет длинное
+    int count = GetCountDigits(number);
+    if (count < pos) return -1;
+    else if (count > pos)
+    {
+        int i = count - pos;
+        while (i > 0)
+        {
+            factor = factor * 10;
+            i--;
+        }
+    }
+    return (number / factor % 10);
+}
+
+Console.Write("Введите число: ");
+int readNumber = Convert.ToInt32(Console.ReadLine());
+
+int digit = GetDigitSetPos(3, readNumber);
+if (digit != -1)
+    Console.Write($"Третья цифра введённого числа: {digit}.");
+else
+    Console.Write("У введённого числа отсутствует третья цифра.");
 
 //------------------------------------------------------------------------
