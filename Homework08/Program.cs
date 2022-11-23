3fe50faddba4bf8024dc4df43aba7ec2f5cbfbdb
Vovqa <vovan102vb@gmail.com>
2022-11-23 23:26:24 +0300

    Added task #60

--- Homework08/Program.cs
+++ Homework08/Program.cs
@@ -61,7 +61,7 @@ int GetMinSumElementsRowInArray(int[,] array)
         if (sumRows[i] < sumRows[idMin]) idMin = i;
 
     return idMin + 1; // нам ведь нужна строка, а не индекс :)
-} */
+}
 
 void QuadMatrixMultiplication(int a1, int a2, int b1, int b2,
                             int c1, int c2, int d1, int d2)
@@ -69,7 +69,35 @@ void QuadMatrixMultiplication(int a1, int a2, int b1, int b2,
     Console.WriteLine("Результат произведения двух матриц: ");
     Console.WriteLine($"|{a1 * c1 + b1 * c2} {a1 * d1 + b1 * d2}|");
     Console.WriteLine($"|{a2 * c1 + b2 * c2} {a2 * d1 + b2 * d2}|\n");
+} */
+
+int[,,] Create3dArrayNoRepeatValue(int rows, int columns, int depth, int minValue, int maxValue)
+{
+    int[,,] array = new int[rows, columns, depth];
+    bool[] valueMemory = new bool[maxValue + 1];
+
+    for (int r = 0; r < rows; r++)
+        for (int c = 0; c < columns; c++)
+            for (int d = 0; d < columns; d++)
+            {
+            ReRandom:
+                int valueNew = new Random().Next(minValue, maxValue + 1);
+                if (valueMemory[valueNew]) goto ReRandom;
+                valueMemory[valueNew] = true;
+                array[r, c, d] = valueNew;
+            }
+
+    return array;
 }
+
+void Show3dArrayInt(int[,,] array)
+{
+    for (int r = 0; r < array.GetLength(0); r++)
+        for (int c = 0; c < array.GetLength(1); c++)
+            for (int d = 0; d < array.GetLength(2); d++)
+                Console.Write($"{array[r, c, d]}({r},{c},{d})\n");
+}
+
 //--------------------------[ Задачи ]------------------------------------
 /* 
 Задача 54:
@@ -82,7 +110,7 @@ int inputRow = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов: ");
 int inputCol = Convert.ToInt32(Console.ReadLine());
 int min = 10;
-int max = 98;
+int max = 99;
 
 int[,] randArray = CreateRandom2dArrayInt(inputRow, inputCol, min, max);
 Show2dArrayInt(randArray);
@@ -121,7 +149,7 @@ Console.Write("Номер строки с наименьшей суммой эл
 18 20
 15 18
  */
-
+/* 
 Console.WriteLine("Пусть матрицы выглядят так: \n");
 Console.WriteLine("|a1 b1| |c1 d1|");
 Console.WriteLine("|a2 b2| |c2 d2|\n");
@@ -145,6 +173,25 @@ Console.Write("Введите d2: ");
 int d2 = Convert.ToInt32(Console.ReadLine());
 
 QuadMatrixMultiplication(a1, a2, b1, b2, c1, c2, d1, d2);
+ */
+//------------------------------------------------------------------------
+/* 
+Задача 60.
+...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+ */
+
+Console.Write("Введите кол-во строк: ");
+int inputRow = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int inputCol = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите глубину: ");
+int inputDep = Convert.ToInt32(Console.ReadLine());
+int min = 10;
+int max = 99;
+
+int[,,] randArray = Create3dArrayNoRepeatValue(inputRow, inputCol, inputDep, min, max);
+Show3dArrayInt(randArray);
 
 //------------------------------------------------------------------------
 
